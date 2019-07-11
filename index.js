a1231ed1c0aaa371927b91b9e7b746d5c4eeef30
Stephen Belanger <admin@stephenbelanger.com>
2019-07-11 21:06:57 +0800

    v1.0.0

---
+++ index.js
@@ -0,0 +1,56 @@
+function sortEntriesByKey ([ a ], [ b ]) {
+  return a > b ? 1 : -1
+}
+
+function rebuildReducer (target, [ key, value ]) {
+  target[key] = rebuildAsOrdered(value)
+  return target
+}
+
+function rebuildAsOrdered (source) {
+  let target
+
+  if (Array.isArray(source)) {
+    target = []
+  } else if (typeof source === 'object') {
+    target = {}
+  } else {
+    return source
+  }
+
+  return Object.entries(source)
+    .sort(sortEntriesByKey)
+    .reduce(rebuildReducer, target)
+}
+
+function labelsToKey (labels) {
+  return JSON.stringify(rebuildAsOrdered(labels))
+}
+
+class ObjectIdentityMap extends Map {
+  has (labels) {
+    return super.has(labelsToKey(labels))
+  }
+
+  get (labels) {
+    return super.get(labelsToKey(labels))
+  }
+
+  set (labels, value) {
+    return super.set(labelsToKey(labels), value)
+  }
+
+  delete (labels) {
+    return super.delete(labelsToKey(labels))
+  }
+
+  ensure (labels, build) {
+    const key = labelsToKey(labels)
+    if (!super.has(key)) {
+      super.set(key, build(labels))
+    }
+    return super.get(key)
+  }
+}
+
+module.exports = ObjectIdentityMap
